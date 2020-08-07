01c391277c7b0bd6badac6dce99f186627232985
sayantika98 <60005526+sayantika98@users.noreply.github.com>
2020-08-08 04:20:31 +0530

    Changed index.html and added mail.php for a working contact form

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])){
+
+
+
+$name = $_POST['name'];
+$emailFrom = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Subject: $subject \n Message: $message";
+$subjectNew = "CONTACT FORM";
+$recipient = "sayantikaghosh98@gmail.com";
+$mailheader = "From: ".$emailFrom;
+mail($recipient, $subjectNew, $formcontent, $mailheader) or die("Error!");
+header("Location: index.php?mailsend");
+// echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+}
+?>
